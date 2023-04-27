fceff1ebe79aa8cadaaf8c91ed1e4ca0b444b579
Bryan-Abreu <djflights@gmail.com>
2023-04-27 00:51:39 -0700

    made a hero, styled it and placed it on the home page

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { Button, Container, MainHeading } from '../../globalSytles';
+import {HeroVideo, HeroSection, HeroText, ButtonWrapper, HeroButton} from './HeroStyles';
+import Video from '../../videos/detailing.mp4'
+
+
+const Hero = () => {
+  return (
+    <HeroSection>
+        <HeroVideo autoPlay loop muted src={Video} type='video/mp4'/>
+        <Container>
+            <MainHeading>Your mobile detailing needs</MainHeading>
+            <HeroText>We proivde the best cleaning services in the vally</HeroText>
+            <ButtonWrapper>
+                <Link to='/signup'>
+                <Button>Get Started</Button>
+                </Link>
+                <HeroButton>Find More</HeroButton>
+            </ButtonWrapper>
+        </Container>
+    </HeroSection>
+  )
+}
+
+export default Hero
