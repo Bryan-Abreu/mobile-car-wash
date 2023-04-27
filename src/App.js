12fa648b10b9df3dad4979e441a2dae278c359bc
Bryan-Abreu <djflights@gmail.com>
2023-04-26 23:12:17 -0700

    Made a global style sheet, home page, price page, and sing up page.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+import GlobalStyle from "./globalSytles";
+//----------Pages---------->
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Pricing from "./pages/Pricing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <BrowserRouter>
+      <GlobalStyle /> 
+      <Routes>
+        <Route path="/" element={<Home/>} ></Route>
+        <Route path="/signup" element={<SignUp/>} ></Route>
+        <Route path="/pricing" element={<Pricing/>} ></Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
