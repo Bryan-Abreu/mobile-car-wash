4f1b71a18e7ef54315747688f623a80d901f9361
Bryan-Abreu <djflights@gmail.com>
2023-05-15 21:41:41 -0700

    changed the background video

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,19 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { Button, Container, MainHeading } from '../../globalSytles';
 import {HeroVideo, HeroSection, HeroText, ButtonWrapper, HeroButton} from './HeroStyles';
-import Video from '../../videos/detailing.mp4'
+import Video from '../../videos/foamcar.mp4'
 
 
 const Hero = () => {
   return (
     <HeroSection>
         <HeroVideo autoPlay loop muted src={Video} type='video/mp4'/>
-        <Container>
-            <MainHeading>Your mobile detailing needs</MainHeading>
-            <HeroText>We proivde the best cleaning services in the valley</HeroText>
+        <Container >
+            <MainHeading>J&R Mobile Car Wash And Detailing LLC</MainHeading>
+            <HeroText>Convenient Auto Detailing Wherever You Are!</HeroText>
             <ButtonWrapper>
                 <Link to='/signup'>
-                <Button>Get Started</Button>
+                <Button>Make An Appointment</Button>
                 </Link>
                 <HeroButton>Find More</HeroButton>
             </ButtonWrapper>
